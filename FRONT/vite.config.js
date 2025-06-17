8981df8313ffc27cd0e6f711935979a2f7ddc4d3
Catalinaaaax <catalinsd805@gmail.com>
2025-06-17 12:37:21 -0500

    configurando vite.config

--- FRONT/vite.config.js
+++ FRONT/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Prueba-tecnica/', // Asegúrate que coincida con el nombre de tu repositorio
 })
